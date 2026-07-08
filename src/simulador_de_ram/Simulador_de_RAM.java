7fbb5a26fdf2e6db126f0c4d643123248b9a113e
Alex31-del <alexandernavarroseminario@gmail.com>
2026-07-08 15:17:19 -0500

    commit inicial

---
+++ src/simulador_de_ram/Simulador_de_RAM.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simulador_de_ram;
+
+/**
+ *
+ * @author user
+ */
+public class Simulador_de_RAM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
