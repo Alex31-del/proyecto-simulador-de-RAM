8e3b32048072ae9e82383fda7350fee1d7a4224d
Alex31-del <alexandernavarroseminario@gmail.com>
2026-07-09 14:18:22 -0500

    Actualizacion

---
+++ src/simulador_de_ram/PanelProcesos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simulador_de_ram;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+public class PanelProcesos extends JPanel{
+    
+   private JLabel lNombre, lMemoria,  lAlgoritmo;
+   private JTextField tNombre, tMemoria;
+   private JButton bCrear, bEliminar;
+   private JComboBox<String> cAlgoritmo;
+   private JTable tProceso;
+   private DefaultTableModel modelo;
+   
+   
+}
