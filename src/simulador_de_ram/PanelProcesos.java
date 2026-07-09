27d5a7704b2f9aa6e81fbeba27f23489e8b886e1
Alex31-del <alexandernavarroseminario@gmail.com>
2026-07-09 14:47:32 -0500

    Actualizacion02

--- src/simulador_de_ram/PanelProcesos.java
+++ src/simulador_de_ram/PanelProcesos.java
@@ -5,6 +5,8 @@
 package simulador_de_ram;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
 public class PanelProcesos extends JPanel{
     
    private JLabel lNombre, lMemoria,  lAlgoritmo;
@@ -14,6 +16,46 @@ public class PanelProcesos extends JPanel{
    private JTable tProceso;
    private DefaultTableModel modelo;
    private MemoriaRAM memoria;
+
+    public PanelProcesos(MemoriaRAM memoria) {
+        
+        this.memoria = memoria;
+        
+        setLayout(new BorderLayout());
+        
+        JPanel datos = new JPanel(new GridLayout(4,2));
+        lNombre = new JLabel("Nombre:");
+        tNombre  = new JTextField();
+        
+        lMemoria = new JLabel("Memoria (MB):");
+        tMemoria = new JTextField();
+        
+        lAlgoritmo = new JLabel("Algoritmo:");
+        
+        cAlgoritmo =new JComboBox<>();
+        cAlgoritmo.addItem("First Fit");
+        cAlgoritmo.addItem("Best Fist");
+        cAlgoritmo.addItem("Best Fist");
+        
+        bCrear = new JButton("Crear");
+        bEliminar = new JButton("Eliminar");
+        
+        datos.add(lNombre);
+        datos.add(tNombre);
+        
+        datos.add(lMemoria);
+        datos.add(tMemoria);
+        
+        datos.add(lAlgoritmo);
+        datos.add(cAlgoritmo);
+        
+        datos.add(bCrear);
+        datos.add(bEliminar);
+       
+        add(datos, BorderLayout.NORTH);
+        
+    }
+   
    
    
 }
